ac8ed8e8622e20c599d17c0191418ef6c3b578cb
gjrumney <gjrumney@gmail.com>
2025-07-05 19:37:44 +0100

    Update main.py
    Included a small commentary on the data

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import string
-
-# Take an input
-# Assign each letter a morse code cou
-
 morse_code_dict = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 
     'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 
@@ -19,7 +14,7 @@ morse_code_dict = {
     '@': '.--.-.'
 }
 
-
+# decodes ENG input
 def decode_eng(message):
     post = []
     
@@ -33,10 +28,11 @@ def decode_eng(message):
 
     return post
 
+# delivers MORSE output
 def deliver_morse(post):
     print(' '.join(post))
     
-
+# decodes MORSE input
 def decode_morse(message):
     post = []
     
@@ -52,15 +48,18 @@ def decode_morse(message):
     
     return post
 
+# delivers ENG output
 def deliver_eng(post):
     print(''.join(post))            
 
+# controls ENG -> MORSE sequence
 def eng_morse(message):
     print('\n')
     post = decode_eng(message)
     morse = deliver_morse(post)
     print('\n')
-    
+
+# controls MORSE -> ENG sequence
 def morse_eng(message):
     print('\n')
     message = error_handler(message)
@@ -68,6 +67,7 @@ def morse_eng(message):
     eng = deliver_eng(post)
     print('\n')
 
+# a seemingly superfluous attempt at error handling
 def error_handler(message):
     
     for m in message:
@@ -83,6 +83,12 @@ def main():
         eng_or_morse = input('Type your message: ')
         
         message = list(eng_or_morse)
+
+        '''
+        Process determines whether the language is in morse or English
+        It splits the input into a list and measures the length of its first item
+        If the item is one it might be 'E:.', so it checks - if not, it is still English
+        '''
         
         if len(eng_or_morse[0]) == 1:
             if eng_or_morse[0] == '.':
