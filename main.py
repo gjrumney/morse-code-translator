3a433f8f9d0ca059a98dd41d1b4a49782b23fde7
gjrumney <gjrumney@gmail.com>
2025-07-05 19:27:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+import string
+
+# Take an input
+# Assign each letter a morse code cou
+
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 
+    'Z': '--..', '': '-----', '1': '.----', '2': '..---', 
+    '3': '...--', '4': '....-', '5': '.....', '6': '-....', 
+    '7': '--...', '8': '---..', '9': '----.', '.': '.-.-.-', 
+    ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', 
+    '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...', 
+    ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', 
+    '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', 
+    '@': '.--.-.'
+}
+
+
+def decode_eng(message):
+    post = []
+    
+    for p in message:
+        if p != ' ':
+            key = p.upper()
+            post.append(morse_code_dict[key])
+        else:
+            space = p.replace(' ', '/')
+            post.append(space)
+
+    return post
+
+def deliver_morse(post):
+    print(' '.join(post))
+    
+
+def decode_morse(message):
+    post = []
+    
+    for p in message:
+        if p != '/':
+            for key, val in morse_code_dict.items():
+                if val == p:
+                    letter = key
+                    post.append(letter)
+        else:
+            space = p.replace('/', ' ')
+            post.append(space)
+    
+    return post
+
+def deliver_eng(post):
+    print(''.join(post))            
+
+def eng_morse(message):
+    print('\n')
+    post = decode_eng(message)
+    morse = deliver_morse(post)
+    print('\n')
+    
+def morse_eng(message):
+    print('\n')
+    message = error_handler(message)
+    post = decode_morse(message)
+    eng = deliver_eng(post)
+    print('\n')
+
+def error_handler(message):
+    
+    for m in message:
+        for val in morse_code_dict.items():
+            if val != m:
+                print('This message is invalid.')
+                return None
+
+
+   
+def main():
+    while True:
+        eng_or_morse = input('Type your message: ')
+        
+        message = list(eng_or_morse)
+        
+        if len(eng_or_morse[0]) == 1:
+            if eng_or_morse[0] == '.':
+                message = str(''.join(message))
+                message = message.split(' ')
+                morse_eng(message)
+            else:
+                eng_morse(message)
+        else:
+            morse_eng(message)  
+
+if __name__ == '__main__':
+    main()
+
+        
+    
+    
